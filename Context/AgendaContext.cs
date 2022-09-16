5c9970dbbb2b6958d48a04c36a10bdcef39560e4
Evandro Petrus <evandro_petrus@outlook.com>
2022-09-15 23:46:21 -0300

    CreatedAtAction on Create Method added.

--- Context/AgendaContext.cs
+++ Context/AgendaContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using API01.Entities;
+
 namespace API01.Context
 {
     public class AgendaContext : DbContext
